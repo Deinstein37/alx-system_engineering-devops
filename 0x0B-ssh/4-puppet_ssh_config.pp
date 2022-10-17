a70ff0d2ddf6bdbf42bdbdf4128caa9690ff5d3e
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-10-17 10:46:40 +0100

    Update 4-puppet_ssh_config.pp

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
-# client SSH configuration file so that you can connect to a server without typing a password.
-file_line { 'private key':
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
-  ensure => 'present',
+# sets up a client SSH configuration file so that we can connect to a server without using a password
+include stdlib
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/holberton',
+  replace => true,
 }
 
-file_line { 'refuse to authenticate':
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
-  ensure => 'present',
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
 }
